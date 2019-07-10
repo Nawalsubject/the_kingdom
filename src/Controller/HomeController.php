5b97c59a76881abfa655a233b341101eba21ea91
Nawal Zakarya <nawal.zakarya@gmail.com>
2019-07-10 17:29:39 +0200

    Stylization of the header

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/home", name="app_index")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
