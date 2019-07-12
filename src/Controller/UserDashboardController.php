23704ec44ceaabb48b0692de300b14f4970941ec
Nawal Zakarya <nawal.zakarya@gmail.com>
2019-07-12 21:38:49 +0200

    Stylization of the card + dropdown menu

--- src/Controller/UserDashboardController.php
+++ src/Controller/UserDashboardController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/user")
+ * Class UserDashboardController
+ * @package App\Controller
+ */
 class UserDashboardController extends AbstractController
 {
     /**
-     * @Route("/user/dashboard", name="user_dashboard")
+     * @Route("/dashboard", name="user_dashboard")
      */
     public function index()
     {
@@ -18,10 +24,24 @@ class UserDashboardController extends AbstractController
 
         $godChildren = $user->getGodChildren();
 
-        return $this->render('user_dashboard/index.html.twig', [
+        return $this->render('user/index.html.twig', [
             'user' => $user,
             'buddy' => $buddy,
             'godChildren' => $godChildren,
+        ]);
+    }
+
+    /**
+     * @Route("/listing", name="user_listing")
+     * @param UserRepository $userRepository
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function userListing(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('user/userListing.html.twig', [
+            'users' => $users,
         ]);
     }
 }
